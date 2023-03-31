649276a2f26619f70a90127604aafadf2e3ceef2
mewmew-tea <kd1285579@st.kobedenshi.ac.jp>
2023-03-31 10:48:17 +0900

    Fix uniform buffer

--- main.cpp
+++ main.cpp
@@ -36,8 +36,8 @@ const char shaderCode[] = R"(
     };
     struct UbObject
     {
-        Pos : vec3<f32>,
-        Padding : f32,
+        Pos : vec2<f32>,
+        Padding : vec2<f32>,
     };
     @group(0) @binding(0) var<uniform> ubObject : UbObject;
 
